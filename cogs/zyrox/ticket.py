168debef8b848041761253cec4e63ae346c30427
saturogames7-svg <saturogames7@gmail.com>
2026-07-28 02:06:50 +0300

    Update ticket.py

--- cogs/zyrox/ticket.py
+++ cogs/zyrox/ticket.py
@@ -1,20 +1,15 @@
 import discord
 from utils.emoji import TICKET
 from discord.ext import commands
-
-
 class _ticket(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
     """Ticket"""
     def help_custom(self):
         emoji = TICKET
         label = "Ticket"
         description = "Show you Commands of Ticket"
         return emoji, label, description
-
     @commands.group()
     async def __Ticket__(self, ctx: commands.Context):
-        """`/ticket setup`, `/ticket panels`, `/ticket category add`, `/ticket category remove`, `/ticket color`, `/ticket close`, `/ticket lock`, `/ticket claim`, `/ticket unlock`, `/ticket transcript`"""
-        
+        """`/ticket setup`, `/ticket panels`, `/ticket delete`, `/ticket category add`, `/ticket category remove`, `/ticket color`, `/ticket close`, `/ticket lock`, `/ticket claim`, `/ticket unlock`, `/ticket transcript`"""
