181f1087e3c76ccf0a986fd4264ddd158e2cf33a
saturogames7-svg <saturogames7@gmail.com>
2026-07-27 13:25:01 +0300

    Update logging.py

--- cogs/zyrox/logging.py
+++ cogs/zyrox/logging.py
@@ -1,19 +1,16 @@
 import discord 
 from utils.emoji import CAST
 from discord .ext import commands 
-
 class _logging (commands .Cog ):
     def __init__ (self ,bot ):
         self .bot =bot 
-
     """Logging commands"""
-
     def help_custom (self ):
 		      emoji =CAST
 		      label ="Logging Commands"
 		      description ="Shows you the commands of logging"
 		      return emoji ,label ,description 
-
     @commands .group ()
     async def __Logging__ (self ,ctx :commands .Context ):
-        """`log`, `log enable`, `log disable`, `log config`, `log ignore`, `log status`, `log toggle`"""
+        """`log setup` , `log setall` , `log status` , `log config` , `log toggle` , `log ignore` , `log search` , `log test` , `log export` , `log reset`"""
+		
