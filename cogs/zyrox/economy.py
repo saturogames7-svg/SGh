1b123a73a520719c5fd68ca94304d65f8f118eed
saturogames7-svg <saturogames7@gmail.com>
2026-09-05 15:50:16 +0300

    Update economy commands to include trivia command

--- cogs/zyrox/economy.py
+++ cogs/zyrox/economy.py
@@ -1,14 +1,20 @@
 import discord
 from discord.ext import commands
+
+
 class _economy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
     """Economy"""
+
     def help_custom(self):
         emoji = "🪙"
         label = "Economy"
         description = "Show you Commands of Economy"
         return emoji, label, description
+
     @commands.group()
     async def __Economy__(self, ctx: commands.Context):
-        """`/economy balance`, `/economy leaderboard`, `/economy pay`, `/economy give-money`, `/economy remove-money`, `/economy inventory`, `/economy store list`, `/economy store add`, `/economy store remove`, `/economy store buy`"""
+        """`/economy balance`, `/economy leaderboard`, `/economy pay`, `/economy give-money`, `/economy remove-money`, `/economy inventory`, `/economy trivia`, `/economy store list`, `/economy store add`, `/economy store remove`, `/economy store buy`"""
+        
