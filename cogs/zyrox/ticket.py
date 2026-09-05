a509948197151f333448430e186281373e21ad9c
saturogames7-svg <saturogames7@gmail.com>
2026-09-05 03:21:00 +0300

    Update ticket.py

--- cogs/zyrox/ticket.py
+++ cogs/zyrox/ticket.py
@@ -12,5 +12,4 @@ class _ticket(commands.Cog):
         return emoji, label, description
     @commands.group()
     async def __Ticket__(self, ctx: commands.Context):
-        """`/ticket setup`, `/ticket panels`, `/ticket delete`, `/ticket category add`, `/ticket category remove`, `/ticket color`, `/ticket close`, `/ticket lock`, `/ticket claim`, `/ticket unlock`, `/ticket transcript`, `/ticket add`"""
-        
+        """`/ticket setup`, `/ticket panels`, `/ticket delete`, `/ticket category add`, `/ticket category remove`, `/ticket color`, `/ticket close`, `/ticket lock`, `/ticket claim`, `/ticket unlock`, `/ticket transcript`, `/ticket add`, `/ticket remove`"""
